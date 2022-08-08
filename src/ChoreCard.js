fab362ae2e75b192b6d193cbbfd25e9b8278e4d8
Austin Longo <austinlongocontracting@gmail.com>
2022-08-08 01:40:31 -0400

    added checkbox to chore cards

--- src/ChoreCard.js
+++ src/ChoreCard.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function ChoreCard({chore}) {
+    const [checked, setChecked] = useState(false)
 
-
+    function handleCheck() {
+        setChecked(!checked)
+        console.log(checked)
+    }
     return(
         <li>
             <ul>
-                <li>{chore.choreName}</li>
-                <li>{chore.choreRoom}</li>
+                <li>
+                {chore.choreName}
+                <input 
+                type="checkbox"
+                checked={checked}
+                onChange={handleCheck}
+
+                /> 
+                <button onClick={() => console.log(checked)}>log</button>
+                </li>
+                {/* <li>{chore.choreRoom}</li>
                 <li>{chore.choreTime + " mins"}</li>
-                <li>{chore.choreFrequency + "days"}</li>
+                <li>{chore.choreFrequency + "days"}</li> */}
                 {/* <input type="checkbox">{chore.isCompleted}</input> */}
                 
             </ul>        
